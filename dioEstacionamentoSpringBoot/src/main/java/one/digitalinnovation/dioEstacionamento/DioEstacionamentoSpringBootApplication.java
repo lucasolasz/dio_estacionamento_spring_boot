58caed01adb935d8d65a45262f5174d4f6ad7c57
lucas <lucas@lucas-VirtualBox>
2022-11-03 17:14:56 -0300

    Primeiro commit

---
+++ dioEstacionamentoSpringBoot/src/main/java/one/digitalinnovation/dioEstacionamento/DioEstacionamentoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.dioEstacionamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioEstacionamentoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioEstacionamentoSpringBootApplication.class, args);
+	}
+
+}
